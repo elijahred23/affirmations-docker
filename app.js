caa423da2fe3b9b68dc6e22343e7877b198972ab
elijahred23 <elijahred23@gmail.com>
2023-10-14 15:46:11 -0500

    Eli-3: Fixed cors error

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const affirmation = require('./affirmation');
+const cors = require('cors');
+
 const port = 3000;
 
 const app = express();
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: true}))
 
